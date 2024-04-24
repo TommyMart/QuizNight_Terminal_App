586ba96130932d9b20532c0c2a86d7c2aa4415b2
TommyMart <15423@coderacademy.edu.au>
2024-04-24 13:25:43 +0930

    added csv list that takes topic and score

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+# System Packages
+import os.path
 
+# External Packages
+
+# Import our own functions 
 from quiz_functions import quiz_topics, random_quiz, top_scores, create_new
 
 name = input("\nPlease enter your name: ")
 
 print(f"\nHello {name}, and welcome to the Music Quiz!\n")
-print("Please ensure your audio settings are turned on and set to a moderate volume.\n")
 
+file_name = "list.csv"
+
+# If the file doesn't exist we must create it with correct headings and then close it
+if (not os.path.isfile(file_name)):
+    print("creating file")
+    high_scores = open(file_name, "w")
+    high_scores.write("topic,score")
+    high_scores.close()
+    
 def menu():
     
     print("1. Enter 1 for quiz topics")
     print("2. Enter 2 for top scores")
-    print("3. Enter 3 to start a random quiz")
+    print("3. Enter 3 to quick start a random quiz")
     print("4. Enter 4 to create a new text quiz")
     print("5. Enter 5 to exit quiz")
 
     user_choice = input("\nPlease enter your selection: \n")
     return user_choice
+
+
+
 
 choice = ""
 
